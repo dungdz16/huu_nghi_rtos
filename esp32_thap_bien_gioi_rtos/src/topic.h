43b80cc3fd100035f50409242f80d15183b76dfe
dungdz16 <dung16bkhn@gmail.com>
2022-01-11 16:40:13 +0700

    push border tower

---
+++ esp32_thap_bien_gioi_rtos/src/topic.h
@@ -0,0 +1,30 @@
+#ifndef _TOPIC_H_
+#define _TOPIC_H_
+            /***********************************************************************/
+            /*************************Node Environment*******************************/
+            /***********************************************************************/
+#define node_environment_properties_temperature_payload "mandevices/thap_bien_gioi/device_0001/environment/temperature"
+
+#define node_environment_properties_humidity_payload "mandevices/thap_bien_gioi/device_0001/environment/humidity"
+
+#define node_environment_properties_co_gas_payload "mandevices/thap_bien_gioi/device_0001/environment/co_gas"
+
+#define node_environment_properties_soil_moisture_payload "mandevices/thap_bien_gioi/device_0001/environment/soil_humid"
+
+#define node_environment_properties_rain_payload "mandevices/thap_bien_gioi/device_0001/environment/rain"
+
+#define node_environment_properties_dust_payload "mandevices/thap_bien_gioi/device_0001/environment/dust"
+
+            /***********************************************************************/
+            /*************************Node Device*******************************/
+            /***********************************************************************/
+
+#define node_device_properties_location_payload "mandevices/thap_bien_gioi/device_0001/device/location"
+
+#define node_device_properties_alert_payload "mandevices/thap_bien_gioi/device_0001/device/alert"
+#define node_device_properties_alert_command "mandevices/thap_bien_gioi/device_0001/device/alert/set"
+
+#define node_device_properties_cylinder_payload "mandevices/thap_bien_gioi/device_0001/device/cylinder"
+#define node_device_properties_cylinder_command "mandevices/thap_bien_gioi/device_0001/device/cylinder/set"
+
+#endif
